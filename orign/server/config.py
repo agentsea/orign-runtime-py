29a6af33a52aa3d1ec2a4eb2a2c308281105efb5
Patrick Barker <patrickbarkerco@gmail.com>
2024-10-27 16:19:55 -0600

    redis

--- orign/server/config.py
+++ orign/server/config.py
@@ -9,9 +9,11 @@ class Config:
         if value is None:
             raise ValueError(f"Required environment variable {key} is not set")
         return value
+    
     # QUEUE configurations
-    BOOTSTRAP_SERVERS = _get_required_env("QUEUE_BOOTSTRAP_SERVERS").split(",")
-    QUEUE_TYPE = _get_required_env("QUEUE_TYPE")
+    BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092").split(",")
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+    QUEUE_TYPE = _get_required_env("QUEUE_TYPE").lower()
     INPUT_TOPICS = _get_required_env("QUEUE_INPUT_TOPICS").split(",")
     OUTPUT_TOPIC = _get_required_env("QUEUE_OUTPUT_TOPIC")
     GROUP_ID = _get_required_env("QUEUE_GROUP_ID")
